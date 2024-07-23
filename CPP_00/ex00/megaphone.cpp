2c67041f31185b4f8460d8bd9a5541a6d30999c0
Boussard Fanny <boussard.fanny@gmail.com>
2024-07-23 10:21:36 +0200

    ex00 OK

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+# include <string>
+
+int main(int ac, char **av)
+{
+	std::string const myString = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	if (ac == 1)
+		std::cout << myString;
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return 0;
+}
