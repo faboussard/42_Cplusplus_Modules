325ef96fc1c92d1938e9240d6b747f332d71da3e
Boussard Fanny <boussard.fanny@gmail.com>
2024-08-06 00:33:27 +0200

    content is copied

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -1,47 +1,41 @@
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
 
-/*
- * Forbidden functions : std::string::replace
-Create a program that takes three parameters in the following order: a filename and
-two strings, s1 and s2.
-It will open the file <filename> and copies its content into a new file
-<filename>.replace, replacing every occurrence of s1 with s2.
-Using C file manipulation functions is forbidden and will be considered cheating. All
-the member functions of the class std::string are allowed, except replace. Use them
-wisely!
-Of course, handle unexpected inputs and errors. You have to create and turn in your
-own tests to ensure your program works as expected.
- */
 int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cerr << " Please enter 3 arguments: a filename and two strings" << std::endl;
+		std::cerr << "Please enter exactly three arguments: a filename and 2 strings" << std::endl;
 		return 1;
 	}
-	for (int i = 1; i < argc; i++)
+	std::string inputFileName(argv[1]);
+	if (inputFileName.length() > 1000)
 	{
-		if (std::string(argv[i]).length() > 1000)
-		{
-			std::cerr << " String is too long. arguments should be of 1000 characters max" << std::endl;
-			return 1;
-		}
+		std::cerr << "Filename is too long. It should be of 1000 characters max" << std::endl;
+		return 1;
 	}
-	std::ifstream myfile(argv[1]);
-
+	std::string outputFileName = inputFileName + "_replace";
+	std::ifstream infile(inputFileName.c_str());
+	if (!infile.is_open())
+	{
+		std::cerr << "Unable to open input file: " << inputFileName << std::endl;
+		return 1;
+	}
+	std::ofstream outfile(outputFileName.c_str());
+	if (!outfile.is_open())
+	{
+		std::cerr << "Unable to open output file: " << outputFileName << std::endl;
+		infile.close();
+		return 1;
+	}
 	std::string line;
-	if (myfile.is_open())
+	while (std::getline(infile, line))
 	{
-		while ( getline (myfile,line) )
-		{
-			std::cout << line << '\n';
-		}
-		myfile.close();
+		outfile << line << '\n';
 	}
-	else std::cout << "Unable to open file" << std::endl;
-
-	std::ofstream newfile;
-//	newfile.ft_replace();
+	infile.close();
+	outfile.close();
+	std::cout << "Content copied from " << inputFileName << " to " << outputFileName << std::endl;
+	return 0;
 }
