f22d3dc445672aef5f299567e07f953cf08ef163
Boussard Fanny <boussard.fanny@gmail.com>
2024-08-06 15:26:28 +0200

    exo 05 ok

--- module_01/ex05/Harl.hpp
+++ module_01/ex05/Harl.hpp
@@ -1,6 +1,19 @@
 #ifndef CPP_HARL_HPP
 #define CPP_HARL_HPP
 
+#include <string>
+#include <iostream>
 
+class Harl
+{
+private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+
+public:
+	void complain( std::string level);
+};
 
 #endif //CPP_HARL_HPP
