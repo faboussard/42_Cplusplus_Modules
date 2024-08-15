9319eb197bfe35eb6bf2faa5e76c917be0af58e6
faboussa <boussard.fanny@gmail.com>
2024-08-15 19:20:56 +0200

    iex01 ok

--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -34,33 +34,3 @@ public:
 };
 
 #endif
-
-/*#include "Fixed.hpp"
-
-Fixed::Fixed() : _rawBits(0) {
-  std::cout << "Default constructor called" << std::endl;
-}
-
-Fixed::Fixed(const Fixed &copy) {
-  std::cout << "Copy constructor called" << std::endl;
-  *this = copy;
-}
-
-Fixed &Fixed::operator=(const Fixed &src) {
-  std::cout << "Copy assignment operator called" << std::endl;
-  this->_rawBits = src.getRawBits();
-  return (*this);
-}
-
-Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
-
-int Fixed::getRawBits(void) const {
-  std::cout << "getRawBits member function called" << std::endl;
-  return (this->_rawBits);
-}
-
-void Fixed::setRawBits(int const raw) {
-  std::cout << "setRawBits member function called" << std::endl;
-  this->_rawBits = raw;
-}
-*/
