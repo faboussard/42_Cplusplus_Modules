96bb2d7440402ea04dd98d43f6094e084079ba2f
Boussard Fanny <boussard.fanny@gmail.com>
2024-08-22 12:47:26 +0200

    ajout ex fixed point

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -55,20 +55,11 @@ void Fixed::setRawBits(int const raw) {
 ///*////////////////////////////////////////
 float Fixed::toFloat(void) const {
   return (((float)_rawBits / (float)(1 << _frac_bit)));
-} // le bitshifing se fait sur le 1 puis on divise par ce chiffre total de
-  // bits.Supposons que _rawBits = 42 et _frac_bit = 8. Alors, le diviseur sera
-  // 1 << 8 = 256. La division de 42 par 256 donne environ 0,1640625.
-  // L'expression (1 << _frac_bit) déplace les bits du nombre 1 _frac_bit
-  // positions vers la gauche. Cela crée un nombre dont tous les bits après la
-  // première position sont des zéros, et le nombre de bits après la virgule est
-  // égal à _frac_bit. Par exemple, si _frac_bit = 8, alors 1 << _frac_bit donne
-  // 256 (en notation binaire, 100000000).
+} // un int divise pqr 1 donne un float. on le shift de 2 puissance 8.
 
 int Fixed::toInt(void) const {
   return (_rawBits >> _frac_bit);
-  // ex: [ 42 \times 2^{8} = 2688 ]
-  // si on a 42.1 et quon decale les bits vers la guache on aura le 1 en forme
-  // binaire qui va dispaitre
+//le raw bi test un int; sii je le shift ca fait un int
 }
 ////////////////////////////////////* NEW
 ///*////////////////////////////////////////
--- module_02/ex01/main.cpp
+++ module_02/ex01/main.cpp
@@ -1,19 +1,28 @@
 #include "Fixed.hpp"
 #include <iostream>
 
-int main(void) {
-  Fixed a;
-  Fixed const b(10);
-  Fixed const c(42.42f);
-  Fixed const d(b);
-  a = Fixed(1234.4321f);
-  std::cout << "a is " << a << std::endl;
-  std::cout << "b is " << b << std::endl;
-  std::cout << "c is " << c << std::endl;
-  std::cout << "d is " << d << std::endl;
-  std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-  return 0;
+int main(void)
+{
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+	Fixed e = Fixed(5);
+	a = Fixed(318.28f);
+
+	std::cout << "round of 10859.52 is " << roundf(10859.52) << std::endl;
+
+	std::cout << "a is " << a << std::endl; // on obient 318.281 car ya 1 de bruit qui sest cree dans la conversion
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "e is " << e << std::endl; // on obient 318.281 car ya 1 de bruit qui sest cree dans la conversion
+	std::cout << "e is " << e.toInt() << std::endl; // on obient 318.281 car ya 1 de bruit qui sest cree dans la conversion
+	std::cout << "e is " << e.toFloat() << std::endl; // on obient 318.281 car ya 1 de bruit qui sest cree dans la conversion
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return 0;
 }
