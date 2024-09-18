c0d1f780bc76b689258a5a842e0493c5791e8871
faboussa <boussard.fanny@gmail.com>
2024-09-18 15:27:23 +0200

    specialValuesAreFloat function added

--- module_06/ex00/ScalarConverter.cpp
+++ module_06/ex00/ScalarConverter.cpp
@@ -3,93 +3,99 @@
 #include <iostream>
 
 /*============================================================================*/
-/*       Constructors                                                           */
+/*       Constructors */
 /*============================================================================*/
 
 ScalarConverter::ScalarConverter() {}
 
 ScalarConverter::ScalarConverter(const ScalarConverter &other) {
-		*this = other;
+  *this = other;
 }
 
 ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other) {
-	if (this != &other) {}
-	return *this;
+  if (this != &other) {
+  }
+  return *this;
 }
 
 ScalarConverter::~ScalarConverter() {}
 
 /*============================================================================*/
-/*       member functions                                                      */
+/*       member functions */
 /*============================================================================*/
 
-void ScalarConverter::convert(const std::string& input) {
-	const std::string specialValues[] = {"-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
-	const size_t specialCount = sizeof(specialValues) / sizeof(specialValues[0]);
+bool specialValuesAreFloat(const std::string &input) {
+  return (input == "-inff" || input == "+inff" || input == "nanf");
+}
 
-	std::string charRepresentation;
-	int intRepresentation;
-	float floatRepresentation;
-	double doubleRepresentation;
+void ScalarConverter::convert(const std::string &input) {
+  const std::string specialValues[] = {"-inff", "+inff", "nanf",
+                                       "-inf",  "+inf",  "nan"};
+  const size_t specialCount = sizeof(specialValues) / sizeof(specialValues[0]);
 
-	/*              special value case              */
+  std::string charRepresentation;
+  int intRepresentation;
+  float floatRepresentation;
+  double doubleRepresentation;
 
-	for (size_t i = 0; i < specialCount; ++i) {
-		if (input == specialValues[i])
-		{
-			std::cout << "char: impossible" << std::endl;
-			std::cout << "int: impossible" << std::endl;
-			std::cout << "float: " << input << std::endl;
-			std::cout << "double: " << (input.substr(0, input.length() - 1)) << std::endl;
-			return;
-		}
-	}
+  /*              special value case              */
 
-	/*              single char case              */
+  for (size_t i = 0; i < specialCount; ++i) {
+    if (input == specialValues[i]) {
+      std::cout << "char: impossible" << std::endl;
+      std::cout << "int: impossible" << std::endl;
+      std::cout << "float: " << input << std::endl;
+      if (specialValuesAreFloat(input))
+        std::cout << "double: " << (input.substr(0, input.length() - 1))
+                  << std::endl;
+      else
+        std::cout << "double: " << input << std::endl;
+      return;
+    }
+  }
 
-	if (input.length() == 1 && std::isprint(input[0]) && !std::isdigit(input[0])) {
-		charRepresentation = "'" + input + "'";
-		intRepresentation = static_cast<int>(input[0]);
-		floatRepresentation = static_cast<float>(input[0]);
-		doubleRepresentation = static_cast<double>(input[0]);
+  /*           		char           */
 
-		std::cout << "char: " << charRepresentation << std::endl;
-		std::cout << "int: " << intRepresentation << std::endl;
-		std::cout << "float: " << floatRepresentation << ".0f" << std::endl;
-		std::cout << "double: " << doubleRepresentation << ".0" << std::endl;
-		return;
-	}
+  if (input.length() == 1 && std::isprint(input[0]) &&
+      !std::isdigit(input[0])) {
+    charRepresentation = static_cast<std::string>("'" + input + "'");
+    intRepresentation = static_cast<int>(input[0]);
+    floatRepresentation = static_cast<float>(input[0]);
+    doubleRepresentation = static_cast<double>(input[0]);
 
-	/*              several char case             */
+    std::cout << "char: " << charRepresentation << std::endl;
+    std::cout << "int: " << intRepresentation << std::endl;
+    std::cout << "float: " << floatRepresentation << ".0f" << std::endl;
+    std::cout << "double: " << doubleRepresentation << ".0" << std::endl;
+    return;
+  }
 
+  /*              strings           */
 
-	bool isFloat = (input.length() > 0 && input[input.length() - 1] == 'f');
-	if (isFloat)
-	{
-		floatRepresentation = std::atof(input.c_str());
-		doubleRepresentation = static_cast<double>(floatRepresentation);
-	} else
-	{
-		doubleRepresentation = std::atof(input.c_str());
-		floatRepresentation = static_cast<float>(doubleRepresentation);
-	}
-	intRepresentation = static_cast<int>(doubleRepresentation);
-	if (intRepresentation >= 32 && intRepresentation <= 126) {
-		charRepresentation = "'" + std::string(1, static_cast<char>(intRepresentation)) + "'";
-	}
-	else
-	{
-		charRepresentation = "Non displayable";
-	}
+  bool isFloat = (input.length() > 0 && input[input.length() - 1] == 'f');
+  if (isFloat) {
+    floatRepresentation = std::atof(input.c_str());
+    doubleRepresentation = static_cast<double>(floatRepresentation);
+  } else {
+    doubleRepresentation = std::atof(input.c_str());
+    floatRepresentation = static_cast<float>(doubleRepresentation);
+  }
+  intRepresentation = static_cast<int>(doubleRepresentation);
+  if (intRepresentation >= 32 && intRepresentation <= 126) {
+    charRepresentation =
+        "'" + std::string(1, static_cast<char>(intRepresentation)) + "'";
+  } else {
+    charRepresentation = "Non displayable";
+  }
 
-	std::cout << "char: " << charRepresentation << std::endl;
-	std::cout << "int: " << intRepresentation << std::endl;
+  std::cout << "char: " << charRepresentation << std::endl;
+  std::cout << "int: " << intRepresentation << std::endl;
 
-	if (floatRepresentation == static_cast<int>(floatRepresentation)) {
-		std::cout << "float: " << floatRepresentation << ".0f" << std::endl;
-		std::cout << "double: " << doubleRepresentation << ".0" << std::endl;
-	} else {
-		std::cout << "float: " << floatRepresentation << "f" << std::endl;
-		std::cout << "double: " << doubleRepresentation << std::endl;
-	}
+  if (floatRepresentation == static_cast<int>(floatRepresentation)) {
+    std::cout << "float: " << floatRepresentation << ".0f" << std::endl;
+    std::cout << "double: " << doubleRepresentation << ".0" << std::endl;
+  } else {
+    std::cout << "float: " << floatRepresentation << "f" << std::endl;
+    std::cout << "double: " << doubleRepresentation << std::endl;
+  }
 }
--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -1,16 +1,16 @@
 #include "ScalarConverter.hpp"
 
 #include <iostream>
+#include <limits>
 
-int main(int argc, char *argv[])
-{
-	if (argc == 1)
-	{
-		std::cout << "Error: no parameters\n" << "Usage: <program> <literal>" << std::endl;
-		return 1;
-	}
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    std::cout << "Error: no parameters\n"
+              << "Usage: <program> <literal>" << std::endl;
+    return 1;
+  }
 
-	ScalarConverter::convert(argv[1]);
+  ScalarConverter::convert(argv[1]);
 
-	return 0;
+  return 0;
 }
