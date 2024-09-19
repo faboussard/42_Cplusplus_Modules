3fb61cc32ba81417608d8d8c5a06346593beb149
faboussa <boussard.fanny@gmail.com>
2024-09-19 13:15:24 +0200

    added copy test

--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -65,45 +65,73 @@ int main() {
       std::cerr << "Exception: " << e.what() << std::endl;
     }
   }
-  //   std::cout << "\n\t\tOTHER OUT OF RANGE TESTS\n" << std::endl;
-  //   {
-  //     try {
-  //       Array<int> a(-2);
-  //     } catch (std::exception &e) {
-  //       std::cerr << "Exception: " << e.what() << std::endl;
-  //     }
-  //   }
-  //   try {
-  //     Array<int> a(MAX_VALUE);
-  //     a[MAX_VALUE] = 0;
-  //   } catch (std::exception &e) {
-  //     std::cerr << "Exception: " << e.what() << std::endl;
-  //   }
-  //   std::cout << "\n\t\tDEEP COPY TESTS\n" << std::endl;
-  //   try {
-  //     {
-  //       Array<float> f(5);
-  //       Array<float> copy(f);
+  std::cout << "\n\t\tOTHER OUT OF RANGE TESTS\n" << std::endl;
+  {
+    try {
+      //   Array<int> a(-2);
+      // } catch (std::exception &e) {
+      //   std::cerr << "Exception: " << e.what() << std::endl;
+      //   }
+      // }
+      //   try {
+      Array<int> a(MAX_VALUE);
+      a[MAX_VALUE] = 0;
+    } catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    std::cout << "\n\t\tDEEP COPY TESTS\n" << std::endl;
+    try {
+      {
+        Array<float> f(5);
+        Array<float> copy(f);
+        Array<float> copy2(10);
 
-  //       std::cout << "array f is : ";
-  //       printArray(f);
+        std::cout << "array f is : ";
+        printArray(f);
+        std::cout << std::endl;
 
-  //       std::cout << "array copy from f is : ";
-  //       printArray(copy);
+        std::cout << "array copy from f is : ";
+        printArray(copy);
+        std::cout << std::endl;
 
-  //       for (unsigned int i = 0; i < copy.size(); i++) {
-  //         copy[i] = 5;
-  //       }
-  //       std::cout << "all values of copy array are now 5. " << std::endl;
+        for (unsigned int i = 0; i < copy.size(); i++) {
+          copy[i] = 5;
+        }
+        std::cout << "all values of copy array are now 5. " << std::endl;
 
-  //       std::cout << "array f is : ";
-  //       printArray(f);
+        std::cout << "array copy from f is : ";
+        printArray(copy);
+        std::cout << std::endl;
 
-  //       std::cout << "array copy from f is : ";
-  //       printArray(copy);
-  //     }
-  //   } catch (std::exception &e) {
-  //     std::cerr << "Exception: " << e.what() << std::endl;
-  //   }
+        std::cout << "array f is : ";
+        printArray(f);
+        std::cout << std::endl;
+      }
+    } catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    std::cout << "\n\tSIZE COPY TESTS\n" << std::endl;
+    try {
+      {
+        Array<float> f(5);
+        Array<float> copy(10);
+
+        std::cout << std::endl;
+        std::cout << " size of f: " << f.size() << std::endl;
+        std::cout << " size of copy: " << copy.size() << std::endl;
+
+        std::cout << "\n\t copy2 = f \n" << std::endl;
+
+        copy = f;
+        std::cout << std::endl;
+        std::cout << " size of f: " << f.size() << std::endl;
+        std::cout << " size of copy: " << copy.size() << std::endl;
+
+        std::cout << std::endl;
+      }
+    } catch (std::exception &e) {
+      std::cerr << "Exception: " << e.what() << std::endl;
+    }
+  }
   return 0;
 }
