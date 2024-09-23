cc0cd0b46eeaadba0a8be6fb5652fc06e2203132
Boussard Fanny <boussard.fanny@gmail.com>
2024-09-23 10:38:09 +0200

    parsing

--- module_09/ex01/main.cpp
+++ module_09/ex01/main.cpp
@@ -5,8 +5,17 @@
 #include "RPN.hpp"
 #include <cctype>
 #include <cstring>
+#include <iostream>
+#include <algorithm>
 
-void processRPN(const std::string& expression)
+void checkArgsCount(int argc);
+bool isOperator(char token);
+void checkOpOrDigits(std::string &argv);
+std::string remove_all_spaces(const std::string &s);
+void checkDuplicatedOP(std::string &argv);
+void checkArgv(std::string &argv);
+
+void processRPN(const std::string &expression)
 {
 	RPN rpn;
 
@@ -29,13 +38,12 @@ void processRPN(const std::string& expression)
 			{
 				rpn.calculate(op);
 			}
-			catch (const std::runtime_error& e)
+			catch (const std::runtime_error &e)
 			{
 				std::cerr << "Error: " << e.what() << std::endl;
 				return;
 			}
-		}
-		else
+		} else
 		{
 			count++;
 			if (count == 2)
@@ -56,12 +64,31 @@ void processRPN(const std::string& expression)
 	if (!rpn.empty())
 	{
 		std::cout << "Result: " << rpn.top() << std::endl;
-	}
-	else
+	} else
 	{
 		std::cerr << "Error: The RPN expression is empty" << std::endl;
 	}
 }
+
+
+int main(int argc, char *argv[])
+{
+	checkArgsCount(argc);
+	std::string expression = argv[1];
+	checkArgv(expression);
+	try
+	{
+		processRPN(expression);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
+////////////////////////////////////////////UTILS//////////////////////////////////////////////////////////////
 
 void checkArgsCount(int argc)
 {
@@ -72,73 +99,83 @@ void checkArgsCount(int argc)
 	}
 }
 
-bool isOperator(const std::string &token) {
-	return (token == "+" || token == "-" || token == "*" || token == "/");
+bool isOperator(char token)
+{
+	return (token == '+' || token == '-' || token == '*' || token == '/');
 }
 
-void checkOpOrDigits(char **argv) {
+void checkOpOrDigits(std::string &argv)
+{
 	int i = 1;
 
-	while (argv[i]) {
-		std::string arg = argv[i];
+	while (argv[i])
+	{
 
-		if (isOperator(arg)) {
-			i++;
-			continue;
-		}
-
-		for (size_t j = 0; j < arg.length(); ++j) {
-			if (!isdigit(arg[j]) && !(arg[j] == '-' && j == 0)) {
-				std::cerr << "Error: invalid expression (invalid token: " << arg << ")" << std::endl;
-				exit(EXIT_FAILURE);
-			}
+		if (!isdigit(argv[i]) && i == 0)
+		{
+			std::cerr << "Error: invalid expression (invalid token: " << argv << ")" << std::endl;
+			exit(EXIT_FAILURE);
 		}
 		i++;
 	}
 }
 
-void checkOpCount(char **argv) {
+std::string remove_all_spaces(const std::string &s)
+{
+	std::string result = s;
+	result.erase(std::remove_if(result.begin(), result.end(), ::isspace), result.end());
+	return result;
+}
+
+
+
+void checkDuplicatedOP(std::string &argv)
+{
+	int i = 1;
+
+	std::string trimmed;
+	trimmed = remove_all_spaces(argv);
+	while (trimmed[i] && trimmed[i + 1])
+	{
+		if (isOperator(trimmed[i]) && (isOperator(trimmed[i + 1]) || trimmed[i + 1 ] == '\0'))
+		{
+			std::cerr << "Error: invalid expression (operator is duplicated " << trimmed[i] << ")" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+}
+
+void checkOpCount(std::string &argv)
+{
 	int countOp = 0;
 	int countNonOp = 0;
 	int i = 1;
 
-	while (argv[i]) {
-		std::string arg = argv[i];
-
-		if (isOperator(arg)) {
+	while (argv[i])
+	{
+		if (isOperator(argv[i]))
 			countOp++;
-		} else {
+		else
 			countNonOp++;
-		}
 		i++;
 	}
-
-	if (countOp == 0) {
+	if (countOp == 0)
+	{
 		std::cerr << "Error: no operator found in expression" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
-	if (countOp >= countNonOp) {
+	if (countOp >= countNonOp)
+	{
 		std::cerr << "Error: invalid expression (more operators than operands)" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 }
 
-void checkArgs(int argc, char **argv) {
-	checkArgsCount(argc);
+void checkArgv(std::string &argv)
+{
+	checkDuplicatedOP(argv);
 //	checkOpOrDigits(argv);
 	checkOpCount(argv);
-}
-
-int main(int argc, char* argv[]) {
-	checkArgs(argc, argv);
-	std::string expression = argv[1];
-	try {
-		processRPN(expression);
-	}
-	catch (const std::exception & e) {
-		std::cerr << "Error: " << e.what() << std::endl;
-		return 1;
-	}
-	return 0;
 }
