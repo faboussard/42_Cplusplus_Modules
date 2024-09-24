3f2ec06665f8c36d765f8bda6ccaed70671aa2d3
faboussa <boussard.fanny@gmail.com>
2024-09-24 12:12:19 +0200

    pend to s instead of s to pend

--- module_09/ex02/PmergeMe.cpp
+++ module_09/ex02/PmergeMe.cpp
@@ -1,65 +1,58 @@
-#include <cstdlib>
-#include <iostream>
-#include <stdexcept>
-#include <cstring>
-#include <cerrno>
-#include <limits>
-#include <vector>
-#include <algorithm>
 #include "PmergeMe.hpp"
+#include <algorithm>
+#include <cerrno>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <vector>
 
 /*============================================================================*/
-/*       Constructors 			   	                                        */
+/*       Constructors */
 /*============================================================================*/
 
-PmergeMe::PmergeMe() : _vector(), _s(), _pend(), _deq(), _straggler(0), _sortedPairs()
-{}
+PmergeMe::PmergeMe()
+    : _vector(), _s(), _pend(), _deq(), _straggler(-1), _sortedPairs() {}
 
-PmergeMe::PmergeMe(int argc, char **argv) : _straggler(0), _sortedPairs()
-{
-	if (argc < 3)
-	{
-		std::cerr << "usage: ./PmergeMe int1 int2 ... " << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		char *end;
-		errno = 0;
-		unsigned long value = std::strtoul(argv[i], &end, 10);
-		if (end == argv[i] || *end != '\0' || errno == ERANGE)
-		{
-			std::cerr << "Invalid argument: " << argv[i] << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		if (value > std::numeric_limits<unsigned int>::max())
-		{
-			std::cerr << "value out of range"  << argv[i] << std::endl;
-			exit(EXIT_FAILURE);
-		}
+PmergeMe::PmergeMe(int argc, char **argv) : _straggler(-1), _sortedPairs() {
+  if (argc < 3) {
+    std::cerr << "usage: ./PmergeMe int1 int2 ... " << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  for (int i = 1; i < argc; i++) {
+    char *end;
+    errno = 0;
+    unsigned long value = std::strtoul(argv[i], &end, 10);
+    if (end == argv[i] || *end != '\0' || errno == ERANGE) {
+      std::cerr << "Invalid argument: " << argv[i] << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    if (value > std::numeric_limits<unsigned int>::max()) {
+      std::cerr << "value out of range" << argv[i] << std::endl;
+      exit(EXIT_FAILURE);
+    }
 
-		unsigned int unsignedValue = static_cast<unsigned int>(value);
-		_vector.push_back(unsignedValue);
-		_deq.push_back(unsignedValue);
-	}
+    unsigned int unsignedValue = static_cast<unsigned int>(value);
+    _vector.push_back(unsignedValue);
+    _deq.push_back(unsignedValue);
+  }
 }
 
-PmergeMe::PmergeMe(const PmergeMe &rhs) : _vector(rhs._vector), _s(rhs._s),
-										  _pend(rhs._pend), _deq(rhs._deq), _straggler(rhs._straggler), _sortedPairs(rhs._sortedPairs)
-{}
+PmergeMe::PmergeMe(const PmergeMe &rhs)
+    : _vector(rhs._vector), _s(rhs._s), _pend(rhs._pend), _deq(rhs._deq),
+      _straggler(rhs._straggler), _sortedPairs(rhs._sortedPairs) {}
 
-PmergeMe &PmergeMe::operator=(const PmergeMe &rhs)
-{
-	if (this != &rhs)
-	{
-		_vector = rhs._vector;
-		_deq = rhs._deq;
-		_straggler = rhs._straggler;
-		_sortedPairs = rhs._sortedPairs;
-		_s = rhs._s;
-		_pend = rhs._pend;
-	}
-	return *this;
+PmergeMe &PmergeMe::operator=(const PmergeMe &rhs) {
+  if (this != &rhs) {
+    _vector = rhs._vector;
+    _deq = rhs._deq;
+    _straggler = rhs._straggler;
+    _sortedPairs = rhs._sortedPairs;
+    _s = rhs._s;
+    _pend = rhs._pend;
+  }
+  return *this;
 }
 
 PmergeMe::~PmergeMe() {}
@@ -68,189 +61,137 @@ PmergeMe::~PmergeMe() {}
 /*       Getter Functions                                                    */
 /*============================================================================*/
 
-const vector &PmergeMe::getMyVector() const
-{
-	return _vector;
-}
+const vector &PmergeMe::getMyVector() const { return _vector; }
 
-const deque &PmergeMe::getMyDeque() const
-{
-	return _deq;
-}
+const deque &PmergeMe::getMyDeque() const { return _deq; }
 
-unsigned int PmergeMe::getMyStraggler() const
-{
-	return _straggler;
-}
+unsigned int PmergeMe::getMyStraggler() const { return _straggler; }
 
-const sortedPairs &PmergeMe::getSortedPairs() const {
-	return _sortedPairs;
-}
+const sortedPairs &PmergeMe::getSortedPairs() const { return _sortedPairs; }
 
-vector &PmergeMe::getMyVector() {
-	return _vector;
-}
+vector &PmergeMe::getMyVector() { return _vector; }
 
-deque &PmergeMe::getMyDeque() {
-	return _deq;
-}
+deque &PmergeMe::getMyDeque() { return _deq; }
 
-unsigned int PmergeMe::getMyStraggler() {
-	return _straggler;
-}
+unsigned int PmergeMe::getMyStraggler() { return _straggler; }
 
-sortedPairs &PmergeMe::getSortedPairs() {
-	return _sortedPairs;
-}
+sortedPairs &PmergeMe::getSortedPairs() { return _sortedPairs; }
 
 /*============================================================================*/
-/*       Vector Member Functions                                                    */
+/*       Vector Member Functions */
 /*============================================================================*/
 
-void PmergeMe::createStraggler()
-{
-	if (getMyVector().size() % 2 != 0)
-	{
-		_straggler = _vector.back();
-		_vector.pop_back();
-	}
+void PmergeMe::createStraggler() {
+  if (getMyVector().size() % 2 != 0) {
+    _straggler = _vector.back();
+    _vector.pop_back();
+  }
 }
 
-void PmergeMe::makeSortedPairs()
-{
-	size_t size = _vector.size();
+void PmergeMe::makeSortedPairs() {
+  size_t size = _vector.size();
 
-	for (size_t i = 0; i < size; i += 2)
-	{
-		if (i + 1 < size)
-		{
-			if (_vector[i] < _vector[i + 1])
-				_sortedPairs.push_back(std::make_pair(_vector[i], _vector[i + 1]));
-			else
-				_sortedPairs.push_back(std::make_pair(_vector[i + 1], _vector[i]));
-		}
-	}
-	// TO DELETE
-	std::cout << "Sorted pairs:" << std::endl;
-	for (size_t j = 0; j < _sortedPairs.size(); ++j) {
-		std::cout << "(" << _sortedPairs[j].first << ", " << _sortedPairs[j].second << ")" << std::endl;
-	}
+  for (size_t i = 0; i < size; i += 2) {
+    if (i + 1 < size) {
+      if (_vector[i] < _vector[i + 1])
+        _sortedPairs.push_back(std::make_pair(_vector[i], _vector[i + 1]));
+      else
+        _sortedPairs.push_back(std::make_pair(_vector[i + 1], _vector[i]));
+    }
+  }
 }
 
-/* changer en recursif. est ce que ca reduit le nombre doperation ? en iteratif : 71 us
+/* changer en recursif. est ce que ca reduit le nombre doperation ? en iteratif
+ * : 46 us
  *
  * Recursively sort the [n/2] larger elements from each pair,
- * creating a sorted sequence S of [n/2] of the input elements, in ascending order.*/
-void PmergeMe::s_sortedLargestValues()
-{
-	for (size_t i = 0; i < _sortedPairs.size(); ++i)
-	{
-		_s.push_back(_sortedPairs[i].second);
-	}
+ * creating a sorted sequence S of [n/2] of the input elements, in ascending
+ * order.*/
+void PmergeMe::s_sortedLargestValues() {
+  for (size_t i = 0; i < _sortedPairs.size(); ++i) {
+    _s.push_back(_sortedPairs[i].second);
+  }
 }
 
-void PmergeMe::pend_sortedSmallestValues()
-{
-	for (size_t i = 0; i < _sortedPairs.size(); ++i)
-	{
-		_pend.push_back(_sortedPairs[i].first);
-	}
+void PmergeMe::pend_sortedSmallestValues() {
+  for (size_t i = 0; i < _sortedPairs.size(); ++i) {
+    _pend.push_back(_sortedPairs[i].first);
+  }
 }
 
 vector PmergeMe::generateJacobsthal() {
-	vector jacobsthal;
-	jacobsthal.push_back(1); // On commence directement par J(1) = 1
-
-	for (unsigned int i = 2; i < _s.size(); ++i) {
-		unsigned int next = jacobsthal[i - 1] + 2 * jacobsthal[i - 2];
-		jacobsthal.push_back(next);
-	}
-
-	return jacobsthal;
+  vector jacobsthal;
+   jacobsthal.push_back(0); 
+  jacobsthal.push_back(1);
+  for (unsigned int i = 2; i < _s.size(); ++i) {
+    unsigned int next = jacobsthal[i - 1] + 2 * jacobsthal[i - 2];
+    jacobsthal.push_back(next);
+  }
+  return jacobsthal;
 }
 
-// Fonction pour insérer les éléments en utilisant la recherche binaire
 void PmergeMe::insertUsingBinarySearch(unsigned int element) {
-	vector::iterator it = std::lower_bound(_pend.begin(), _pend.end(), element);
-	_pend.insert(it, element);
+  vector::iterator it = std::lower_bound(_s.begin(), _s.end(), element);
+  _s.insert(it, element);
 }
 
 void PmergeMe::insertAndMerge() {
-	// Générer les nombres de Jacobsthal en fonction de la taille de _s
-	vector jacobsthal = generateJacobsthal();
+  vector jacobsthal = generateJacobsthal();
 
-	// Insérer le premier élément (celui pairé avec le plus petit dans _s) dans _pend
-	if (!_s.empty()) {
-		insertUsingBinarySearch(_s[0]);
-	}
+  if (!_pend.empty()) {
+    insertUsingBinarySearch(_pend[0]);
+  }
 
-	// Insérer les autres éléments en fonction des nombres de Jacobsthal
-	for (size_t i = 1; i < _s.size(); ++i) {
-		unsigned int jacobsthalIndex = (i < jacobsthal.size()) ? jacobsthal[i] : i; // Sécuriser l'index de Jacobsthal
-		insertUsingBinarySearch(_s[jacobsthalIndex % _s.size()]); // S'assurer de ne pas dépasser la taille de _s
-	}
-
-	_vector = _pend;
-
-	// Affichage de la séquence triée '_pend' pour vérifier le résultat
-	std::cout << "Sorted array after insertions:" << std::endl;
-	for (size_t j = 0; j < _pend.size(); ++j) {
-		std::cout << _pend[j] << " ";
-	}
-	std::cout << std::endl;
+  for (size_t i = 1; i < _pend.size(); ++i) {
+    unsigned int jacobsthalIndex = (i < jacobsthal.size()) ? jacobsthal[i] : i;
+    insertUsingBinarySearch(
+        _pend[jacobsthalIndex % _pend.size()]);
+  }
+  _vector = _s;
 }
 
-
-void PmergeMe::insertStraggler()
-{
-	if (_straggler != 0)
-	{
-		vector::iterator it = std::lower_bound(_vector.begin(), _vector.end(), _straggler);
-		_vector.insert(it, _straggler);
-	}
+void PmergeMe::insertStraggler() {
+  if (_straggler != -1) {
+    vector::iterator it =
+        std::lower_bound(_vector.begin(), _vector.end(), _straggler);
+    _vector.insert(it, _straggler);
+  }
 }
 
-void PmergeMe::sortVector()
-{
-	createStraggler();
-	makeSortedPairs();
-	s_sortedLargestValues();
-	pend_sortedSmallestValues();
-	insertAndMerge();
-	insertStraggler();
+void PmergeMe::sortVector() {
+  createStraggler();
+  makeSortedPairs();
+  s_sortedLargestValues();
+  pend_sortedSmallestValues();
+  insertAndMerge();
+  insertStraggler();
 }
 
 /*============================================================================*/
-/*       deque Member Functions                                                    */
+/*       deque Member Functions */
 /*============================================================================*/
-
 
 /*============================================================================*/
 /*       Class overload                                                       */
 /*============================================================================*/
 
-std::ostream &operator<<(std::ostream &os, const PmergeMe &pmergeMe)
-{
-	os << pmergeMe.getMyVector() << std::endl;
-//	os << pmergeMe.getMyDeque() << std::endl;
+std::ostream &operator<<(std::ostream &os, const PmergeMe &pmergeMe) {
+  os << pmergeMe.getMyVector() << std::endl;
+  //	os << pmergeMe.getMyDeque() << std::endl;
 
-	return os;
+  return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const vector &vec)
-{
-	for (vector::const_iterator it = vec.begin(); it != vec.end(); it++)
-	{
-		os << *it << " ";
-	}
-	return os;
+std::ostream &operator<<(std::ostream &os, const vector &vec) {
+  for (vector::const_iterator it = vec.begin(); it != vec.end(); it++) {
+    os << *it << " ";
+  }
+  return os;
 }
 
-std::ostream &operator<<(std::ostream &os, const deque &deque)
-{
-	for (deque::const_iterator it = deque.begin(); it != deque.end(); it++)
-	{
-		os << *it << " ";
-	}
-	return os;
+std::ostream &operator<<(std::ostream &os, const deque &deque) {
+  for (deque::const_iterator it = deque.begin(); it != deque.end(); it++) {
+    os << *it << " ";
+  }
+  return os;
 }
