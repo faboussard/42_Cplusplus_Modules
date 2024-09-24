87fe6f986f61ae6332bfd9622addcc28cd2a426e
faboussa <boussard.fanny@gmail.com>
2024-09-24 13:12:23 +0200

    h

--- module_04/ex00/Animal.hpp
+++ module_04/ex00/Animal.hpp
@@ -3,24 +3,31 @@
 
 #include <iostream>
 
-#define ANIMAL_MAKE_SOUND \
-	std::cout << "generic animal sound" << std::endl;
+#define ANIMAL_MAKE_SOUND std::cout << "generic animal sound" << std::endl;
 
-class Animal
-{
+class Animal {
 protected:
-	std::string _type;
+  std::string _type;
 
 public:
+  Animal();
+  virtual ~Animal();
+  Animal(const Animal &cpy);
+  Animal &operator=(const Animal &src);
 
-	Animal ();
-	virtual ~Animal();
-	Animal(const Animal &cpy);
-	Animal &operator=(const Animal &src);
-
-	std::string const &getType () const;
-	virtual void makeSound() const;
+  std::string const &getType() const;
+  virtual void makeSound() const;
 };
 
 std::ostream &operator<<(std::ostream &stream, Animal &Animal);
+
+#define DEFAULT_CONSTRUCTOR_MESSAGE                                            \
+  std::cout << "Default Constructor called" << std::endl;
+#define CONSTRUCTOR_MESSAGE std::cout << "Constructor called" << std::endl;
+#define COPY_CONSTRUCTOR_MESSAGE                                               \
+  std::cout << "Copy Constructor called" << std::endl;
+#define COPY_ASSIGNMENT_MESSAGE                                                \
+  std::cout << "Copy Assignment operator called" << std::endl;
+#define DESTRUCTOR_MESSAGE std::cout << "Destructor called" << std::endl;
+
 #endif
